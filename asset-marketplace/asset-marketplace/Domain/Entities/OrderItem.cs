925f0e8f721af1b6f12a6883aa67f1288345c111
Serhii Vintilov <Vintarium77@gmail.com>
2026-01-20 15:32:50 +0100

    Remove hardcoded password from appsettings.json

--- asset-marketplace/asset-marketplace/Domain/Entities/OrderItem.cs
+++ asset-marketplace/asset-marketplace/Domain/Entities/OrderItem.cs
@@ -3,9 +3,9 @@
     public class OrderItem : BaseEntity
     {
         public required Guid OrderId { get; set; }
-        public Order? Order { get; set; }
+        public Order Order { get; set; } = null!;
         public required Guid AssetId { get; set; }
-        public Asset? Asset { get; set; }
+        public Asset Asset { get; set; } = null!;
         public required decimal UnitPrice { get; set; }
     }
 }
