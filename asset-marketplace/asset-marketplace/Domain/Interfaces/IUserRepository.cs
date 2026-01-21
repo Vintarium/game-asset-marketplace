bb127200d8a51c510ec5f9c2cb13978eb8125507
Serhii Vintilov <Vintarium77@gmail.com>
2026-01-21 22:54:35 +0300

    added interface: IUserRepository, added class that implemented this interface - UserRepository, added some crud operation for test

--- asset-marketplace/asset-marketplace/Domain/Interfaces/IUserRepository.cs
+++ asset-marketplace/asset-marketplace/Domain/Interfaces/IUserRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace asset_marketplace.Domain.Interfaces
+﻿using asset_marketplace.Domain.Entities;
+
+namespace asset_marketplace.Domain.Interfaces
 {
     public interface IUserRepository
     {
+        Task<List<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(Guid id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(Guid id);
+
     }
 }
--- asset-marketplace/asset-marketplace/Program.cs
+++ asset-marketplace/asset-marketplace/Program.cs
@@ -1,4 +1,8 @@
+using asset_marketplace.Domain.Interfaces;
 using asset_marketplace.Infrastructure;
+using asset_marketplace.Domain.Entities;
+using asset_marketplace.Domain.Enums;
+using asset_marketplace.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,11 +12,61 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var userRepository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
+
+    var userOne = new User
+    {
+        Id = Guid.NewGuid(),
+        Email = "one@test.com",
+        PasswordHash = "password",
+        Role = UserRole.Buyer
+    };
+
+    var userTwo = new User
+    {
+        Id = Guid.NewGuid(),
+        Email = "two@test.com",
+        PasswordHash = "password",
+        Role = UserRole.Seller
+    };
+
+    var userThree = new User
+    {
+        Id = Guid.NewGuid(),
+        Email = "three@test.com",
+        PasswordHash = "password",
+        Role = UserRole.Admin
+    };
+
+    await userRepository.AddAsync(userOne);
+    await userRepository.AddAsync(userTwo);
+    await userRepository.AddAsync(userThree);
+
+    var AllUsers = await userRepository.GetAllAsync();
+
+    foreach (var user in AllUsers)
+    {
+        Console.WriteLine($"user.Id: {user.Id}, user.Email: {user.Email}, User.Role: {user.Role} , user.CreatedAt: {user.CreatedAt}");
+    }
+
+    var UserById = await userRepository.GetByIdAsync(userThree.Id); 
+
+    if (UserById != null)
+    {
+        Console.WriteLine($"userById.Id: {UserById.Id}, userById.Email: {UserById.Email}, UserById.Role: {UserById.Role} , UserById.CreatedAt: {UserById.CreatedAt}");
+    }
+}
+
 
 if (app.Environment.IsDevelopment())
 {
