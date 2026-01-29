c04095d106d62b6a131487d509dd0091990a2562
Serhii Vintilov <Vintarium77@gmail.com>
2026-01-29 09:33:30 +0100

    feat(user): add CreateUserDto

---
+++ asset-marketplace/asset-marketplace/Application/DTOs/CreateUserDto.cs
@@ -0,0 +1,10 @@
+﻿using asset_marketplace.Domain.Enums;
+
+namespace asset_marketplace.Application.DTOs
+{
+    public record CreateUserDto(
+        string Email,
+        string PAssword,
+        UserRole Role
+    );
+}
