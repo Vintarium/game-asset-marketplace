35b3af4aeb48ccff326757fefae890ad14a00879
Serhii Vintilov <Vintarium77@gmail.com>
2026-02-18 10:55:01 +0100

    refactor(dtos): use modificator "public" for more readable and operator "required" for safely handling null warnings

--- asset-marketplace/asset-marketplace/Application/DTOs/CreateUserDto.cs
+++ asset-marketplace/asset-marketplace/Application/DTOs/CreateUserDto.cs
@@ -1,8 +1,9 @@
 ﻿using asset_marketplace.Domain.Enums;
 
 namespace asset_marketplace.Application.DTOs;
-public record CreateUserDto(
-    string Email,
-    string Password,
-    UserRole Role
-);
+public record CreateUserDto
+{
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+    public required UserRole Role { get; init; }
+}
--- asset-marketplace/asset-marketplace/Application/DTOs/UserDto.cs
+++ asset-marketplace/asset-marketplace/Application/DTOs/UserDto.cs
@@ -1,8 +1,9 @@
 ﻿using asset_marketplace.Domain.Enums;
 
 namespace asset_marketplace.Application.DTOs;
-public record UserDto(
-    Guid Id,
-    string Email,
-    UserRole Role
-);
+public record UserDto
+{
+    public required Guid Id { get; init; }
+    public required string Email { get; init; }
+    public required UserRole Role { get; init; }
+}
