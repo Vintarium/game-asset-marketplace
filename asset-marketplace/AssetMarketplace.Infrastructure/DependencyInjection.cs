6658bd2493777454531053389ee3bcbb370bc735
Serhii Vintilov <Vintarium77@gmail.com>
2026-02-25 14:42:47 +0100

    refactor(infrastructure): add DependencyInjection

---
+++ asset-marketplace/AssetMarketplace.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using AssetMarketplace.Domain.Entities;
+using AssetMarketplace.Domain.Interfaces;
+using AssetMarketplace.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AssetMarketplace.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IRepository<User>, BaseRepository<User>>();
+
+        return services;
+    }
+}
