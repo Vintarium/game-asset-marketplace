d53b4b27b6e1477ce16f89dc6570564332acebe9
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-03 12:59:03 +0100

    feat(application): add validation for CreateUserDto

--- asset-marketplace/AssetMarketplace.Application/DependencyInjection.cs
+++ asset-marketplace/AssetMarketplace.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using AssetMarketplace.Application.Interfaces;
 using AssetMarketplace.Application.Services;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace AssetMarketplace.Application;
 
@@ -8,7 +10,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<IUserService, UserService>();
+        services
+            .AddScoped<IUserService, UserService>()
+            .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
---
+++ asset-marketplace/AssetMarketplace.Application/Validation/CreateUserDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using AssetMarketplace.Application.DTOs;
+using AssetMarketplace.Domain.Constants;
+using FluentValidation;
+
+namespace AssetMarketplace.Application.Validation;
+
+public sealed class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(createUserDto => createUserDto.Email)
+            .NotEmpty().WithMessage("Email cannot be empty")
+            .EmailAddress().WithMessage("Incorrect format");
+
+        RuleFor(createUserDto => createUserDto.Password)
+            .NotEmpty().WithMessage("Password cannot be empty")
+            .MinimumLength(ValidationConstants.MinPasswordLength).WithMessage($"Password can`t be smaller than {ValidationConstants.MinPasswordLength} symbols");
+    }
+}
