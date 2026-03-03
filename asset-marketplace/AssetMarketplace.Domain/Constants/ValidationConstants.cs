3d67744c99c5b0d328b5086040abed0905770fc7
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-03 12:56:51 +0100

    feat(domain): add new constant for password

--- asset-marketplace/AssetMarketplace.Domain/Constants/ValidationConstants.cs
+++ asset-marketplace/AssetMarketplace.Domain/Constants/ValidationConstants.cs
@@ -1,8 +1,10 @@
 ﻿namespace AssetMarketplace.Domain.Constants;
+
 public class ValidationConstants
 {
     public const int MaxNameLength = 100;
     public const int MaxDescriptionLength = 500;
     public const int MaxUrlLength = 500;
     public const int MaxEmailLength = 100;
+    public const int MinPasswordLength = 4;
 }
