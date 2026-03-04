f05eb998c5f01e42b5e215c3e5cbfd4010d6fe6b
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-04 13:34:40 +0100

    feat(infrastructure): add UserRepository implementation

---
+++ asset-marketplace/AssetMarketplace.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using AssetMarketplace.Domain.Entities;
+using AssetMarketplace.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetMarketplace.Infrastructure.Repositories;
+
+public class UserRepository(ApplicationDbContext context) : BaseRepository<User>(context), IUserRepository
+{
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken)
+    {
+        return await _dbSet
+            .AsNoTracking()
+            .FirstOrDefaultAsync(user => user.Email == email, cancellationToken);
+    }
+}
