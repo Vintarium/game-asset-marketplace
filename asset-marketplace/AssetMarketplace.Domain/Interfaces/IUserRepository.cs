1092305baecd7d162d2897097b5a102ce3ccf290
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-04 13:32:20 +0100

    feat(domain): add IUserRepository for special behavior

---
+++ asset-marketplace/AssetMarketplace.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using AssetMarketplace.Domain.Entities;
+
+namespace AssetMarketplace.Domain.Interfaces;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<User?> GetByEmailAsync(string email, CancellationToken ct);
+}
+
