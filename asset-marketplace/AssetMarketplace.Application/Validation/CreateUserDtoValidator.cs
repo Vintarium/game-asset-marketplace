d6c83eed27024c3ac65109f7af3716fff252740d
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-04 14:34:00 +0100

    feat(application): add async uniqueness check to CreateUserValidator

--- asset-marketplace/AssetMarketplace.Application/Validation/CreateUserDtoValidator.cs
+++ asset-marketplace/AssetMarketplace.Application/Validation/CreateUserDtoValidator.cs
@@ -1,16 +1,25 @@
 ﻿using AssetMarketplace.Application.DTOs;
 using AssetMarketplace.Domain.Constants;
+using AssetMarketplace.Domain.Interfaces;
 using FluentValidation;
 
 namespace AssetMarketplace.Application.Validation;
 
 public sealed class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
 {
-    public CreateUserDtoValidator()
+    public CreateUserDtoValidator(IUserRepository userRepository)
     {
         RuleFor(createUserDto => createUserDto.Email)
             .NotEmpty().WithMessage("Email cannot be empty")
-            .EmailAddress().WithMessage("Incorrect format");
+            .EmailAddress().WithMessage("Incorrect format")
+
+            .MustAsync(async (email, CancellationToken) =>
+            {
+                var existingUser = await userRepository.GetByEmailAsync(email, CancellationToken);
+
+                return existingUser is null;
+            })
+            .WithMessage("User with this email already exists");
 
         RuleFor(createUserDto => createUserDto.Password)
             .NotEmpty().WithMessage("Password cannot be empty")
