6420ad9bffc0fa022e2e1722621c3826d72d5288
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-25 14:22:53 +0100

    feat(api): add global model validation and exception filters

---
+++ asset-marketplace/AssetMarketplace.API/Filters/ExeptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AssetMarketplace.API.Filters;
+
+public class ExeptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is InvalidOperationException)
+        {
+            context.Result = new BadRequestObjectResult(new
+            {
+                error = context.Exception.Message,
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
---
+++ asset-marketplace/AssetMarketplace.API/Filters/ValidateModelFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AssetMarketplace.API.Filters;
+
+
+public class ValidateModelFilter : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new BadRequestObjectResult(context.ModelState);
+        }
+    }
+}
