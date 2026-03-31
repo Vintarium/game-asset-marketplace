6c66315c7aaef036248fbac87c51cd8d890cbede
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-31 16:11:03 +0200

    refactor(domain): add class of constants for validation messages

---
+++ asset-marketplace/AssetMarketplace.Domain/Constants/ValidationMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace AssetMarketplace.Domain.Constants
+{
+    public static class ValidationMessages
+    {
+        public const string EmailEmpty = "Email cannot be empty";
+        public const string EmailInvalid = "Incorrect format";
+        public const string RoleInvalid = "A non-existent role was specified. Valid values: 0, 1, 2, 4";
+        public const string PasswordEmpty = "Password cannot be empty";
+        public const string PasswordInvalid = "The minimum password length is";
+    }
+}
