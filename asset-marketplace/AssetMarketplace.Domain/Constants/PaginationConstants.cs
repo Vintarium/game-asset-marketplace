463562c8eb1abb12836002f575ea0413cc7a513c
Serhii Vintilov <Vintarium77@gmail.com>
2026-03-31 14:04:56 +0200

    refactor(domain): make constants class static

--- asset-marketplace/AssetMarketplace.Domain/Constants/PaginationConstants.cs
+++ asset-marketplace/AssetMarketplace.Domain/Constants/PaginationConstants.cs
@@ -1,5 +1,6 @@
 ﻿namespace AssetMarketplace.Domain.Constants;
-public class PaginationConstants
+
+public static class PaginationConstants
 {
     public const int DefaultPageNumber = 1;
     public const int DefaultPageSize = 10;
