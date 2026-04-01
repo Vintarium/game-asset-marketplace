5e249a08455564f685ab92d77ce3b4d2a798d285
Serhii Vintilov <Vintarium77@gmail.com>
2026-04-01 11:22:19 +0200

    refactor(domain): make static methods for validation messages instead of constants

--- asset-marketplace/AssetMarketplace.Domain/Constants/ValidationMessages.cs
+++ asset-marketplace/AssetMarketplace.Domain/Constants/ValidationMessages.cs
@@ -2,10 +2,25 @@
 {
     public static class ValidationMessages
     {
-        public const string EmailEmpty = "Email cannot be empty";
-        public const string EmailInvalid = "Incorrect format";
-        public const string RoleInvalid = "A non-existent role was specified. Valid values: 0, 1, 2, 4";
-        public const string PasswordEmpty = "Password cannot be empty";
-        public const string PasswordInvalid = "The minimum password length is";
+        public static string EmptyValue(string propertyName)
+        {
+            return $"{propertyName}: cannot be empty";
+        }
+
+        public static string InvalidValue(string propertyName)
+        {
+            return $"{propertyName}: invalid value";
+        }
+
+        public static string MinimalLengthValue(string propertyName)
+        {
+            return $"{propertyName}: minimum length: {ValidationConstants.MinPasswordLength}";
+        }
+
+        public static string InvalidRole(string propertyName)
+        {
+            return $"{propertyName}: A non-existent role was specified. Valid values: 0, 1, 2, 4";
+        }
+
     }
 }
