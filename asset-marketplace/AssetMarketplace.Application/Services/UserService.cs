bfc374a7c97b0f34e8fe95dd407a3550c8c3fbe9
Serhii Vintilov <Vintarium77@gmail.com>
2026-04-01 14:14:53 +0200

    Feat/add automapper (#12)
    * chore(application): install automapper libraries
    
    * feat(application): add automapper configuration
    
    * refactor(userservice): implement automapper for methods
    
    * refactor(application): use autoMapper in update method
    
    * refactor(domain): remove extra line

--- asset-marketplace/AssetMarketplace.Application/Services/UserService.cs
+++ asset-marketplace/AssetMarketplace.Application/Services/UserService.cs
@@ -2,23 +2,27 @@
 using AssetMarketplace.Application.Interfaces;
 using AssetMarketplace.Domain.Entities;
 using AssetMarketplace.Domain.Interfaces;
+using AutoMapper;
 
 namespace AssetMarketplace.Application.Services;
 
-public class UserService(IUserRepository userRepository, IPasswordHasher passwordHasher) : IUserService
+public class UserService(
+    IUserRepository userRepository,
+    IPasswordHasher passwordHasher,
+    IMapper mapper) : IUserService
 {
     public async Task<IReadOnlyCollection<UserDto>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
     {
         var users = await userRepository.GetAllAsync(pageNumber, pageSize, cancellationToken);
 
-        return users.Select(user => new UserDto { Id = user.Id, Email = user.Email, Role = user.Role, }).ToList();
+        return mapper.Map<IReadOnlyCollection<UserDto>>(users);
     }
 
     public async Task<UserDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
         var user = await userRepository.GetByIdAsync(id, cancellationToken, asNoTracking: true);
 
-        return user is not null ? new UserDto { Id = user.Id, Email = user.Email, Role = user.Role } : null;
+        return mapper.Map<UserDto>(user);
     }
 
     public async Task<UserDto> CreateAsync(CreateUserDto createUserDto, CancellationToken cancellationToken)
@@ -29,12 +33,14 @@ public class UserService(IUserRepository userRepository, IPasswordHasher passwor
             throw new InvalidOperationException("User with this email already exists");
         }
 
-        var user = new User { Email = createUserDto.Email, PasswordHash = passwordHasher.HashPassword(createUserDto.Password), Role = createUserDto.Role };
+        var user = mapper.Map<User>(createUserDto);
+        user.PasswordHash = passwordHasher.HashPassword(createUserDto.Password);
 
         await userRepository.AddAsync(user, cancellationToken);
 
-        return new UserDto { Id = user.Id, Email = user.Email, Role = user.Role };
+        return mapper.Map<UserDto>(user);
     }
+
     public async Task<UserDto?> UpdateAsync(Guid id, UpdateUserDto updateUserDto, CancellationToken cancellationToken)
     {
         var user = await userRepository.GetByIdAsync(id, cancellationToken, asNoTracking: false);
@@ -43,11 +49,11 @@ public class UserService(IUserRepository userRepository, IPasswordHasher passwor
             return null;
         }
 
-        user.Email = updateUserDto.Email;
+        mapper.Map(updateUserDto, user);
 
         await userRepository.UpdateAsync(user, cancellationToken);
 
-        return new UserDto { Id = user.Id, Email = user.Email, Role = user.Role };
+        return mapper.Map<UserDto?>(user);
     }
 
     public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken)
