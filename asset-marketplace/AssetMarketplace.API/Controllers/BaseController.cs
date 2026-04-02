959c280f0b3f441983a48b282ed3130018b74f35
Serhii Vintilov <Vintarium77@gmail.com>
2026-04-02 16:31:43 +0200

    feat(api): add BaseController with Result to Action mapping

---
+++ asset-marketplace/AssetMarketplace.API/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using AssetMarketplace.Domain.Abstractions;
+using AssetMarketplace.Domain.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssetMarketplace.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseController : ControllerBase
+{
+
+    protected IActionResult HandleResult<T>(Result<T> result)
+    {
+        if (result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
+
+        return MapErrorToResponse(result.Error);
+    }
+
+    protected IActionResult HandleResult(Result result)
+    {
+        if (result.IsSuccess)
+        {
+            return NoContent();
+        }
+
+        return MapErrorToResponse(result.Error);
+    }
+
+    private IActionResult MapErrorToResponse(Error error)
+    {
+        return error switch
+        {
+            var e when e == UserErrors.NotFound =>
+                NotFound(new { error = e.Code, message = e.Message }),
+
+            var e when e == UserErrors.EmailNotUnique =>
+                Conflict(new { error = e.Code, message = e.Message }),
+
+            _ => BadRequest(new { error = error.Code, message = error.Message })
+        };
+    }
+}
