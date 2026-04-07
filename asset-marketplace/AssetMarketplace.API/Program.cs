216bb1ec57884cee5481604bf27180fb202f95c3
Serhii Vintilov <Vintarium77@gmail.com>
2026-04-07 16:54:07 +0200

    feat(api): integrate serilog and add global startup exception handling

--- asset-marketplace/AssetMarketplace.API/Program.cs
+++ asset-marketplace/AssetMarketplace.API/Program.cs
@@ -1,27 +1,50 @@
+using AssetMarketplace.API.Extensions;
 using AssetMarketplace.API.Extentions;
+using AssetMarketplace.API.Middleware;
 using AssetMarketplace.Application.Extensions;
 using AssetMarketplace.Infrastructure.Extensions;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+LoggingExtensions.ConfigureLogging();
 
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication();
-
-builder.Services.AddPresentation();
-
-builder.Services.AddSwaggerDocumentation();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwaggerDocumentation();
+    Log.Information("Application is starting...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddApplication();
+
+    builder.Services.AddPresentation();
+
+    builder.Services.AddSwaggerDocumentation();
+
+    var app = builder.Build();
+
+    app.UseMiddleware<GlobalExceptionMiddleware>();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwaggerDocumentation();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Application start-up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
