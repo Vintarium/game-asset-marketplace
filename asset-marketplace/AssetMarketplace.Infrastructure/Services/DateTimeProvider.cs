e7da69eae7f7c96503bd25766c87b32ed92a7664
Serhii Vintilov <Vintarium77@gmail.com>
2026-04-09 13:26:31 +0200

    feat(infrastructure): implement IDateTimeProvider and register in DI

---
+++ asset-marketplace/AssetMarketplace.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using AssetMarketplace.Application.Interfaces;
+
+namespace AssetMarketplace.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
